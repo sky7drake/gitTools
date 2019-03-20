8e934fc222ab22b49dad60e9040214a8a7292c0f
sky7drake <sky7drake@gmail.com>
2019-03-19 22:52:50 -0700

    add common scripts

---
+++ syncUpstream.bat
@@ -0,0 +1,11 @@
+REM git remote add upstream git://github.com/sdlpal/sdlpal.git
+
+REM git remote -v
+
+git fetch upstream
+
+REM git pull upstream master
+
+git checkout master
+
+git merge upstream/master
